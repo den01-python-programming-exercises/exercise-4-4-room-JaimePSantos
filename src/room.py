caffae7fb8e169a3c1aae5e277a98edd7bc2a744
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-23 18:15:01 +0000

    solution

---
+++ src/room.py
@@ -0,0 +1,6 @@
+class Room:
+  class_code = 0
+  number_of_seats = 0
+  def __init__(self, class_code, number_of_seats):
+    self.class_code = class_code
+    self.number_of_seats = number_of_seats
